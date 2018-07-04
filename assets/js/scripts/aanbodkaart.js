8634e56d73391059a47b29955429d4c385417fbc
Jeffrey Pisa (emonks) <jef@emonks.nl>
2018-07-04 17:25:27 +0200

    [TASK] Aanbodkaart styling

--- assets/js/scripts/aanbodkaart.js
+++ assets/js/scripts/aanbodkaart.js
@@ -1,12 +1,13 @@
 (function($) {
   $( document ).ready(function() {
+
     $('a[data-aanbodfilter]').on('click', function() {
       event.preventDefault();
       $('.block').removeClass('active');
       $(this).addClass('active');
       $('.space .row').removeClass().addClass('row');
       $('.space .row .block').closest('.row').addClass('aanbod');
-      
+
       var aanbodfilter = $(this).attr('data-aanbodfilter');
       var targetelement = $( '.space .' + aanbodfilter );
 
