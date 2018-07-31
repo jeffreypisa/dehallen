d6260ed2bca73ca3182c06c199fda35b80e16c43
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2018-07-31 21:48:01 +0200

    [TASK] Front-end feedback

--- archive.php
+++ archive.php
@@ -37,6 +37,8 @@ else {
 }
 
 $context[ 'day_now' ]  = date('D');
+
+// Voor Ramon: day_filter moet de gekozen dag uit de filter zijn
 $day_filter  = date('D');
 $context[ 'day_filter' ]  = strtolower($day_filter);
 
--- assets/js/scripts/aanbodkaart.js
+++ assets/js/scripts/aanbodkaart.js
@@ -1,5 +1,14 @@
 (function($) {
   $( document ).ready(function() {
+    
+    $(".mod-filterblocks a").mouseover(function() {
+      var aanbodfilter = $(this).attr('data-aanbodfilter');
+      $("a." + aanbodfilter).addClass('opacity1');
+    });
+    
+    $(".mod-filterblocks a").mouseleave(function() {
+      $("a").removeClass('opacity1');
+    });
 
     $('a[data-aanbodfilter]').on('click', function(event) {
       event.preventDefault();
