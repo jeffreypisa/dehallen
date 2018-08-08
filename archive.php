ea09520349ba3e6606316cf6a6db38b6d5cf0e50
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2018-08-08 16:26:50 +0200

    [TASK] Dag waarop gefilterd wordt in cijfers (bijvoorbeeld 12.08.18)

--- archive.php
+++ archive.php
@@ -41,6 +41,9 @@ $context[ 'day_now' ]  = date('D');
 // Onderstaand moet dag doorgeven waarop gefilterd wordt
 $context[ 'day_filter' ]  = date('D');
 
+// Onderstaand moet dag doorgeven waarop gefilterd wordt in cijfers 10.08.18
+$context[ 'date_filter_short' ]  = date('D');
+
 $day_filter  = date('D');
 
 $context[ 'hour_now' ]  = date('H');
