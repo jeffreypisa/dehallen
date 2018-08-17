6cf2f2cff5658525cf782f8ff9d600c4ab6ff8b0
Ramon Fincken <ramon@creativepulses.nl>
2018-08-17 22:31:24 +0200

    [TASK] xml import

---
+++ xml.php
@@ -0,0 +1,46 @@
+<?php
+$xml = file_get_contents( 'xml.xml');
+if( $_GET['type'] == 'events' ) {
+    echo $xml;die();
+}
+
+
+
+$xml = simplexml_load_string( $xml );
+
+$internal_run = time();
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<event_datetimes>';
+$events = $xml->xpath('//event');
+
+if( count( $events ) > 0 ) {
+    foreach( $events as $event ) {
+        $datetimes = $event->xpath('datetimes/datetime');
+        
+        if( count( $datetimes ) > 0 ) {
+            
+            foreach( $datetimes as $datetime ) {
+                
+                echo '<event_datetime>';
+                echo '<internal_run>'.$internal_run.'</internal_run>';
+                
+                $out = '<event_parent_guid>'.$event->guid.'</event_parent_guid>';
+                $out .= '<date_start>'.$datetime->date_start.'</date_start>';
+                $out .= '<time_start>'.$datetime->time_start.'</time_start>';
+                $out .= '<date_end>'.$datetime->date_start.'</date_end>';
+                $out .= '<time_end>'.$datetime->date_start.'</time_end>';
+                $out .= '<is_allowed_after_start>'.$datetime->is_allowed_after_start.'</is_allowed_after_start>';
+                
+                echo $out;
+                
+                echo '<internal_hash>'.md5( $out ) .'</internal_hash>';
+                echo '</event_datetime>';
+                
+            }
+            
+        }        
+    }
+}
+
+echo '</event_datetimes>';
