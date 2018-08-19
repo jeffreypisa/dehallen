8f2457e935f859a753f29f11637121e73fc3f7c0
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2018-08-19 18:06:31 +0200

    [TASK] Responsive improvements

---
+++ assets/js/scripts/iostouchhoverfix.js
@@ -0,0 +1,23 @@
+(function($) {
+  $(document).ready(function() {
+    // tablet "one touch (click)" X "hover" > link redirection
+    $('a').on('touchmove touchend', function(e) {
+    
+        // if touchmove>touchend, set the data() for this element to true. then leave touchmove & let touchend fail(data=true) redirection
+        if (e.type == 'touchmove') {
+            $.data(this, "touchmove_cancel_redirection", true );
+            return;
+        }
+    
+        // if it's a simple touchend, data() for this element doesn't exist.
+        if (e.type == 'touchend' && !$.data(this, "touchmove_cancel_redirection")) {
+            var el = $(this);
+            var link = el.attr('href');
+            window.location = link;
+        }
+    
+        // if touchmove>touchend, to be redirected on a future simple touchend for this element
+        $.data(this, "touchmove_cancel_redirection", false );
+    });
+  });
+}(jQuery));
